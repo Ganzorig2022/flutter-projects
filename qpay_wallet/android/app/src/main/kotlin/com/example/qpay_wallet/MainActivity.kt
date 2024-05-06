96ca177facc2c96e0c49447777e09ec7ecacc223
ganzorig <ganzorig.n@qpay.mn>
2024-05-06 16:20:59 +0800

    new

---
+++ qpay_wallet/android/app/src/main/kotlin/com/example/qpay_wallet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qpay_wallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ qpay_wallet/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final appBootstrap = AppBootstrap();
+
+  runApp(const MyApp());
+}
